01c828c3a1b58641fc9485ad58c711800184ad8a
asolanky <alwinsolanky@gmail.com>
2018-12-12 06:12:58 +0000

    Added start of state manipulation

--- src/App.js
+++ src/App.js
@@ -28,7 +28,16 @@ class App extends Component {
     this.state = {
       list: list,
     };
+
+    this.onDismiss = this.onDismiss.bind(this);
   }
+
+  onDismiss(id) {
+    const updatedList = this.state.list.filter(function isNotId(item) {
+    return item.objectID !== id;
+    });
+  }
+
   render() {
     return (
       <div className="App">
@@ -40,6 +49,14 @@ class App extends Component {
             <span>{item.author}</span>
             <span>{item.num_comments}</span>
             <span>{item.points}</span>
+            <span>
+              <button
+                onClick={() => this.onDismiss(item.objectID)}
+                type="button"
+              >
+                Dismiss
+              </button>
+            </span>
           </div>
         )}
       </div>
