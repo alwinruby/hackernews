5373599ee27c20cbd242f53191fdfe11d1d72113
asolanky <alwinsolanky@gmail.com>
2018-12-13 09:21:42 +0000

    Working dismiss button

--- src/App.js
+++ src/App.js
@@ -33,20 +33,9 @@ class App extends Component {
   }
 
   onDismiss(id) {
-    // const updatedList = this.state.list.filter(function isNotId(item) {
-    // return item.objectID !== id;
-    // });
-
-    // function isNotId(item) {
-    //   return item.objectID !== id;
-    // }
-    // const updatedList = this.state.list.filter(isNotId);
-
-    // const isNotId = item => item.objectID !== id;
-    // const updatedList = this.state.list.filter(isNotId);
-
-    const updatedList = this.state.list.filter(item => item.objectID !== id);
-
+    const isNotId = item => item.objectID !== id;
+    const updatedList = this.state.list.filter(isNotId);
+    this.setState({ list: updatedList });
   }
 
   render() {
