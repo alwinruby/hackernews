b0b99672ff918719028048fbe7e819a3226ac243
asolanky <alwinsolanky@gmail.com>
2018-12-20 06:31:26 +0000

    controlled component

--- src/App.js
+++ src/App.js
@@ -5,18 +5,18 @@ const list = [
   {
     title: 'React',
     url: 'https://facebook.github.io/react/',
-    author: 'Jordan Walke',
-    num_comments: 3,
-    points: 4,
-    objectID: 0,
+    author: ' Jordan Walke ',
+    num_comments: 3 ,
+    points: 4 ,
+    objectID: 0 ,
   },
   {
     title: 'Redux',
     url: 'https://github.com/reactjs/redux',
-    author: 'Dan Abramov, Andrew Clark',
-    num_comments: 2,
-    points: 5,
-    objectID: 1,
+    author: ' Dan Abramov , Andrew Clark ',
+    num_comments: 2 ,
+    points: 5 ,
+    objectID: 1 ,
   },
 ];
 
@@ -57,6 +57,7 @@ class App extends Component {
       <div className="App">
         <form>
           <input type="text"
+          value={searchTerm}
           onChange={this.onSearchChange}
           />
         </form>
