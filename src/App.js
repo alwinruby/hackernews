d84fb6d3d4530b0676b6a61a5437374080986e62
asolanky <alwinsolanky@gmail.com>
2018-12-21 12:51:53 +0000

    Further tidy up the Search

--- src/App.js
+++ src/App.js
@@ -72,10 +72,10 @@ class App extends Component {
 
 class Search extends Component {
   render() {
-    const { value, onChange } = this.props;
+    const { value, onChange, children } = this.props;
     return (
       <form>
-        <input
+        {children} <input
           type="text"
           value={value}
           onChange={onChange}
