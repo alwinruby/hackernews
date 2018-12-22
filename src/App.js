5e63df700dd1b1ca0918228289abe0f02cd1996f
asolanky <alwinsolanky@gmail.com>
2018-12-22 20:17:17 +0000

    added class button

--- src/App.js
+++ src/App.js
@@ -113,4 +113,24 @@ class Table extends Component {
   }
 }
 
+class Button extends Component {
+  render() {
+    const {
+      onClick,
+      className,
+      children,
+    } = this.props;
+
+    return (
+      <button
+        onClick={onClick}
+        className={className}
+        type="button"
+      >
+        {children}
+      </button>
+    );
+  }
+}
+
 export default App;
