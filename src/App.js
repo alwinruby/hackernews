b813dbd462d0b1a22cb9f322032343cdcc8db0e5
asolanky <alwinsolanky@gmail.com>
2018-12-26 11:35:28 +0000

    Table tidied up

--- src/App.js
+++ src/App.js
@@ -35,6 +35,28 @@ const Search = ({ value, onChange, children }) =>
       />
   </form>
 
+  const Table = ({ list, pattern, onDismiss }) =>
+    <div className="table">
+      {list.filter(isSearched(pattern)).map(item =>
+        <div key={item.objectID} className="table-row">
+          <span>
+            <a href={item.url}>{item.title}</a>
+          </span>
+          <span>{item.author}</span>
+          <span>{item.num_comments}</span>
+          <span>{item.points}</span>
+          <span>
+          <Button
+            onClick={() => onDismiss(item.objectID)}
+            className="button-inline"
+          >
+            Dismiss
+          </Button>
+        </span>
+      </div>
+    )}
+  </div>
+
 class App extends Component {
 
   constructor(props){
@@ -75,31 +97,6 @@ class App extends Component {
             pattern={searchTerm}
             onDismiss={this.onDismiss}
           />
-      </div>
-    );
-  }
-}
-
-class Table extends Component {
-  render() {
-    const { list, pattern, onDismiss } = this.props;
-    return (
-      <div>
-        {list.filter(isSearched(pattern)).map(item =>
-          <div key={item.objectID}>
-            <span>
-              <a href={item.url}>{item.title}</a>
-            </span>
-            <span>{item.author}</span>
-            <span>{item.num_comments}</span>
-            <span>{item.points}</span>
-            <span>
-              <Button onClick={() => onDismiss(item.objectID)}>
-                Dismiss
-              </Button>
-            </span>
-          </div>
-        )}
       </div>
     );
   }
