5af2042a22883007db26a21ba0f208778716d09d
asolanky <alwinsolanky@gmail.com>
2018-12-27 10:36:37 +0000

    Table refactored further

--- src/App.js
+++ src/App.js
@@ -20,6 +20,17 @@ const list = [
   },
 ];
 
+const largeColumn = {
+  width: '40%',
+};
+
+const midColumn = {
+  width: '30%',
+};
+const smallColumn = {
+  width: '10%',
+};
+
 function isSearched(searchTerm) {
   return function(item) {
     return item.title.toLowerCase().includes(searchTerm.toLowerCase());
