f02f0de856e6cbd3128e8c0a9f009e42df33d788
asolanky <alwinsolanky@gmail.com>
2019-01-07 17:57:58 +0000

    render on search submit

--- src/App.js
+++ src/App.js
@@ -153,6 +153,7 @@ class App extends Component {
           <Search
             value={searchTerm}
             onChange={this.onSearchChange}
+            onSubmit={this.onSearchSubmit}
           >
           Search
           </Search>
