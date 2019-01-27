76f24277530c2fc1293b134d37f831f2fff2cc1e
asolanky <alwinsolanky@gmail.com>
2019-01-27 07:55:38 +0000

    Updated tests

--- src/App.test.js
+++ src/App.test.js
@@ -1,20 +1,38 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import renderer from 'react-test-renderer';
-import App from './App';
+// import App from './App';
+import App, { Search } from './App';
 
-describe('App', () => {
+// describe('App', () => {
+//
+//   it('renders without crashing', () => {
+//     const div = document.createElement('div');
+//     ReactDOM.render(<App />, div);
+//     ReactDOM.unmountComponentAtNode(div);
+//   });
+//
+//   test('has a valid snapshot', () => {
+//     const component = renderer.create(
+//       <App /> );
+//     let tree = component.toJSON();
+//     expect(tree).toMatchSnapshot();
+//   });
+// });
 
+describe('Search', () => {
+  
   it('renders without crashing', () => {
     const div = document.createElement('div');
-    ReactDOM.render(<App />, div);
-    ReactDOM.unmountComponentAtNode(div);
-  });
+    ReactDOM.render(<Search>Search</Search>, div);
+});
 
   test('has a valid snapshot', () => {
     const component = renderer.create(
-      <App /> );
+      <Search>Search</Search>
+    );
     let tree = component.toJSON();
     expect(tree).toMatchSnapshot();
   });
+
 });
