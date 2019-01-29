5ac9c90651d080a539deb8572232a6f339e60092
asolanky <alwinsolanky@gmail.com>
2019-01-29 07:37:33 +0000

    New test for tables added

--- src/App.test.js
+++ src/App.test.js
@@ -1,24 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import renderer from 'react-test-renderer';
-// import App from './App';
-import App, { Search, Button } from './App';
-
-// describe('App', () => {
-//
-//   it('renders without crashing', () => {
-//     const div = document.createElement('div');
-//     ReactDOM.render(<App />, div);
-//     ReactDOM.unmountComponentAtNode(div);
-//   });
-//
-//   test('has a valid snapshot', () => {
-//     const component = renderer.create(
-//       <App /> );
-//     let tree = component.toJSON();
-//     expect(tree).toMatchSnapshot();
-//   });
-// });
+import App, { Search, Button, Table } from './App';
 
 describe('Search', () => {
 
@@ -52,5 +35,29 @@ describe('Button', () => {
     let tree = component.toJSON();
     expect(tree).toMatchSnapshot();
   });
+
+});
+
+
+describe('Table', () => {
+  const props = {
+    list: [
+      { title: '1', author: '1', num_comments: 1, points: 2, objectID: 'y' },
+      { title: '2', author: '2', num_comments: 1, points: 2, objectID: 'z' },
+    ],
+  };
   
+  it('renders without crashing', () => {
+      const div = document.createElement('div');
+      ReactDOM.render(<Table { ...props } />, div);
+  });
+
+  test('has a valid snapshot', () => {
+
+    const component = renderer.create(
+      <Table { ...props } />
+      );
+    let tree = component.toJSON();
+      expect(tree).toMatchSnapshot();
+  });
 });
