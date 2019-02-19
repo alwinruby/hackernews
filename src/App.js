0a1395670a375170dab81495ffcc6376103d05db
asolanky <alwinsolanky@gmail.com>
2019-02-19 07:02:39 +0000

    sort still not working

--- src/App.js
+++ src/App.js
@@ -232,13 +232,53 @@ class Search extends Component {
   }
 }
 
-const Table = ({ list,
+const Table = ({
+  list,
   sortKey,
   onSort,
-  onDismiss }) =>
+  onDismiss
+}) =>
   <div className="table">
-    {list.map(item =>
-    //{SORTS[sortKey](list).map(item =>
+
+    <div className="table-header">
+      <span style={{ width: '40%' }}>
+        <Sort
+          sortKey={'TITLE'}
+          onSort={onSort}
+          >
+          Title
+        </Sort>
+      </span>
+      <span style={{ width: '30%' }}>
+      <Sort
+          sortKey={'AUTHOR'}
+          onSort={onSort}
+      >
+      Author
+        </Sort>
+      </span>
+      <span style={{ width: '10%' }}>
+        <Sort
+          sortKey={'COMMENTS'}
+          onSort={onSort}
+        >
+          Comments
+        </Sort>
+      </span>
+      <span style={{ width: '10%' }}>
+        <Sort
+          sortKey={'POINTS'}
+          onSort={onSort}
+        >
+          Points
+        </Sort>
+      </span>
+      <span style={{ width: '10%' }}>
+        Archive
+      </span>
+    </div>
+    //{list.map(item =>
+    {SORTS[sortKey](list).map(item =>
       <div key={item.objectID} className="table-row">
         <span style={{ width: '40%' }}>
           <a href={item.url}>{item.title}</a>
