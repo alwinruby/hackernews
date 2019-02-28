fa9a66f841d96a7b1ea396df67202e48f6d6ecc6
asolanky <alwinsolanky@gmail.com>
2019-02-28 00:03:02 +0000

    updated and constants

--- Notes/Chapter6.md
+++ Notes/Chapter6.md
@@ -1,3 +1,7 @@
 #Chapter 6 - State Management in React and beyond
 
 Lifting state.
+
+• React
+    – lift state management up and down to suitable components – setState can use a function to prevent stale state bugs
+    – existing external solutions that help you to tame the state
--- src/App.js
+++ src/App.js
@@ -21,6 +21,25 @@ const SORTS = {
   POINTS: list => sortBy(list, 'points').reverse(),
 };
 
+const updateSearchTopStoriesState = (hits, page) => (prevState) => {
+  const { searchKey, results } = prevState;
+  const oldHits = results && results[searchKey]
+    ? results[searchKey].hits
+    : [];
+  const updatedHits = [
+    ...oldHits,
+    ...hits
+];
+  return {
+    results: {
+      ...results,
+      [searchKey]: { hits: updatedHits, page }
+    },
+    isLoading: false
+  };
+};
+
+
 class App extends Component {
   constructor(props) {
     super(props);
